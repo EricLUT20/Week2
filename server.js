f3b4229c0ccf5c8643830921c2030f50610f3924
EricLUT20 <ericthomasliskii@gmail.com>
2023-11-12 04:05:52 +0200

    3

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 app.get("/hello", (req, res) => {
   res.json({ msg: "Hello world" })
 })
@@ -9,6 +11,12 @@ app.get("/hello", (req, res) => {
 app.get("/echo/:id", (req, res) => {
   let id = req.params.id
   res.json({ id })
+})
+
+app.post("/sum", (req, res) => {
+  let { numbers } = req.body
+  let sum = numbers.reduce((total, number) => total + number, 0)
+  res.json({ sum })
 })
 
 app.listen(port, () => console.log("Server listening on port " + port))
