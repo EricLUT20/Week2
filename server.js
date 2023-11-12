cb37fe9bfb5c719cf8f3fae2c800f314a40ae135
EricLUT20 <ericthomasliskii@gmail.com>
2023-11-12 03:21:31 +0200

    2

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const app = express()
 const port = 3000
 
 app.get("/hello", (req, res) => {
-  console.log("Hello world")
   res.json({ msg: "Hello world" })
+})
+
+app.get("/echo/:id", (req, res) => {
+  let id = req.params.id
+  res.json({ id })
 })
 
 app.listen(port, () => console.log("Server listening on port " + port))
